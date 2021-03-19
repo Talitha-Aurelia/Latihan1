824413f10e0a066232c0cfe4ca397daed7272fea
Talitha-Aurelia <80021785+Talitha-Aurelia@users.noreply.github.com>
2021-03-19 20:47:41 +0700

    Add files via upload

---
+++ HalloIndonesia.java
@@ -0,0 +1,13 @@
+//Nama  : Talitha Aurelia Shakira
+//NIM   : 11200930000023
+//Kelas : 2A
+
+package Hallo;
+
+public class HalloIndonesia {
+   public static void main(String[] args){
+    System.out.println("Hallo Indonesia");
+    System.out.println("Selamat Pagi Indonesia");
+    System.out.println("Jaya Indonesia");
+}
+}
