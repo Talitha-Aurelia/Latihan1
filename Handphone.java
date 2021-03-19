f8bcb514077bc40f27e27f890e69d729646200d1
Talitha-Aurelia <80021785+Talitha-Aurelia@users.noreply.github.com>
2021-03-19 20:48:28 +0700

    Add files via upload

---
+++ Handphone.java
@@ -0,0 +1,20 @@
+//Nama  : Talitha Aurelia Shakira
+//NIM   :11200930000023
+//Kelas :2A
+
+package hp;
+
+public class Handphone {
+    String hidupkan ;
+    String lakukanPanggilan;
+    String kirimSms;
+    String matikan;
+    
+    void kondisiHandphone(){
+        
+     System.out.println("Handphone Hidup"+ hidupkan);
+     System.out.println("Kring, Kring, Kring" + lakukanPanggilan);
+     System.out.println("Dung, Dung" + kirimSms);
+     System.out.println("Handphone mati" + matikan);
+}
+}
---
+++ HandphoneBeraksi.java
@@ -0,0 +1,17 @@
+//Nama  : Talitha Aurelia Shakira
+//NIM   : 11200930000023
+//Kelas : 2A
+
+package hp;
+public class HandphoneBeraksi {
+    
+    public static void main(String[] args){
+        Handphone Status = new Handphone();
+
+            Status.hidupkan = "...";
+            Status.lakukanPanggilan= "...Panggilan Dilakukan";
+            Status.kirimSms = "... SMS berhasil Terkirim";
+            Status.matikan = "...";
+            Status.kondisiHandphone();
+}
+}
