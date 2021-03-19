c3e9a7a85fe97353d4028d1f8d67c5f005c7e8b3
Talitha-Aurelia <80021785+Talitha-Aurelia@users.noreply.github.com>
2021-03-19 20:49:04 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,19 @@
+//Nama : Talitha Aurelia Shakira
+//NIM  : 11200930000023
+//Kelas : 2A
+
+package Siswa;
+
+public class Mahasiswa {
+    String membaca;
+    String nyontek;
+    String modifikasi; 
+    
+    void KondisiMahasiswa(){
+     
+     System.out.println("Mahasiswa terlihat sedang membaca"+ membaca);
+     System.out.println("Ternyata sedang menyontek" + nyontek);
+     System.out.println("Dan melakukan modifikasi" + modifikasi);
+     
+}
+}
---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,16 @@
+//Nama  : Talitha Aurelia Shakira
+//NIM   : 11200930000023
+//Kelas : 2A
+
+package Siswa;
+public class MahasiswaBeraksi {
+    
+    public static void main(String[] args){
+        Mahasiswa Status = new Mahasiswa();
+
+            Status.membaca = "...";
+            Status.nyontek = "...";
+            Status.modifikasi = "...";
+            Status.KondisiMahasiswa();
+}
+}
